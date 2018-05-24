5813ca569fbd0ae0e71d43b2589e4709c3e4009e
Cliff Laschet <>
2018-05-24 17:06:13 +0200

    Changed import

--- src/main/scala/com/functionalai/mountaincar/reinforcementlearning/QFunction.scala
+++ src/main/scala/com/functionalai/mountaincar/reinforcementlearning/QFunction.scala
@@ -1,5 +1,7 @@
 package com.functionalai.mountaincar.reinforcementlearning
 
+import scala.collection.mutable.HashMap
+
 /**
   * A Q function provides utilities (i.e. expected cumulative rewards) for any state, action pair '(s1, a)'.
   * The larger the utility, the more cumulative reward is expected to be obtained in the following states after performing 'a' in 's1'.
@@ -23,13 +25,29 @@ trait QFunction{
 
 /**
   * A tabular Q function provides utilities by tracking past obtained utilities in a table structure.
+  *
   * @param learningRate
   * @param gamma
   */
 class TabularQFunction(learningRate: Double, gamma: Double) extends QFunction {
 
   //The table of utilities
-  val utilities = scala.collection.mutable.HashMap.empty[(State, Action), Double]
+  val utilities = HashMap.empty[(State, Action), Double]
+
+  def utility(state: State, action: Action): Double = {
+
+    //Retrieve the utility of the (state, action) pair from the table.
+    //If it doesn't exist, add it with a random initial value and return this value
+    utilities.get((state, action)) match{
+
+      case Some(utility) => utility
+      case None => {
+        utilities.put((state, action), 1d)
+        1d
+      }
+    }
+
+  }
 
   def add(t: Transition): Unit = {
 
@@ -48,21 +66,6 @@ class TabularQFunction(learningRate: Double, gamma: Double) extends QFunction {
     //Update s1 using the Bellman equation (temporal difference learning)
     val updatedUtility = s1Utility + learningRate * ((t.reward + gamma * bestS2Utility) - s1Utility)
     utilities.put((t.state1, t.action), updatedUtility)
-
-  }
-
-  def utility(state: State, action: Action): Double = {
-
-    //Retrieve the utility of the (state, action) pair from the table.
-    //If it doesn't exist, add it with a random initial value and return this value
-    utilities.get((state, action)) match{
-
-      case Some(utility) => utility
-      case None => {
-        utilities.put((state, action), 1d)
-        1d
-      }
-    }
 
   }
 }
